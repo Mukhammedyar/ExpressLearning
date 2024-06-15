12d5d57dc1b3ddc7a3e2ac207c77fe0caa919421
Mukhammedyar <116536513+Mukhammedyar@users.noreply.github.com>
2024-06-16 03:38:05 +0500

    HTTP Methods 2-lesson 16.06.2024

--- app.js
+++ app.js
@@ -3,6 +3,38 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+app.use(express.json());
+
+//get method
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hell Express" });
+});
+
+//post method
+app.post("/", (req, res) => {
+  //   res.status(200).json({ message: "hello Express" });
+  const { lastName, firstName } = req.body;
+  res.send("My firstName is " + firstName + " and my lastName is " + lastName);
+});
+
+//delete method
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(id + "-item successfully deleted");
+});
+
+//update put method
+app.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.send(
+    id + "-item successfully edited and his body: " + JSON.stringify(body)
+  );
+});
+
+app.get("/post", (req, res) => {
+  res.send("hello Express Post page");
+});
 
 app.listen(PORT, () => {
   console.log("listening on port: hhtp://localhost:" + PORT);
