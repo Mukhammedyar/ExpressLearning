91f3e5f33046e373818f2cbd784f1bf680ef75d4
Mukhammedyar <116536513+Mukhammedyar@users.noreply.github.com>
2024-06-16 16:50:29 +0500

    Data Base ENV file 3-lesson 16.06.2024

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-const http = require("http");
+require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose");
+const postModel = require("./models/post.model");
+const PORT = process.env.PORT || 3000;
+const DATABASE_URL =
+  process.env.DATABASE_URL ||
+  "mongodb+srv://muxadev001:muxammedyar1@cluster0.zzp9qem.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;";
 const app = express();
-const PORT = 3000;
+
 app.use(express.json());
 
 //get method
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "hell Express" });
+app.get("/", async (req, res) => {
+  try {
+    const allPosts = await postModel.find();
+    res.status(200).json(allPosts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 //post method
-app.post("/", (req, res) => {
-  //   res.status(200).json({ message: "hello Express" });
-  const { lastName, firstName } = req.body;
-  res.send("My firstName is " + firstName + " and my lastName is " + lastName);
+app.post("/", async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const newPost = await postModel.create({ title, body });
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 //delete method
@@ -36,6 +50,20 @@ app.get("/post", (req, res) => {
   res.send("hello Express Post page");
 });
 
-app.listen(PORT, () => {
-  console.log("listening on port: hhtp://localhost:" + PORT);
-});
+const connectDataBase = async () => {
+  try {
+    await mongoose
+      .connect(DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("connected to database: "));
+    app.listen(PORT, () => {
+      console.log("listening on port: hhtp://localhost:" + PORT);
+    });
+  } catch (error) {
+    console.log(`error connecting to DataBase: ${error}`);
+  }
+};
+
+connectDataBase();
---
+++ models/post.model.js
@@ -0,0 +1,8 @@
+const { model, Schema } = require("mongoose");
+
+const postSchema = new Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+});
+
+module.exports = model("Post", postSchema);
